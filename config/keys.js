3888923cca9aa6aa7404d5208fc780d4caa6f327
David Woolsey <woolsey2316@gmail.com>
2023-10-20 13:01:36 +1100

    mongodb connection bug

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 module.exports = {
-  mongoURI: `mongodb://localhost/${process.env.DB}`,
+  mongoURI: `mongodb://127.0.0.1:27017/${process.env.DB}`,
   ACCESS_TOKEN_SECRET: "secret_access",
   REFRESH_TOKEN_SECRET: "secret_refresh",
 };
